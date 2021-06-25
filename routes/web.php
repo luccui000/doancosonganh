d95d4c27b1d6ddfa569741ccee5900314ad5be00
Luc Cui <minhluc15.ok@gmail.com>
2021-06-25 09:47:32 +0700

    add: test mail

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
- 
+
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
  
+Route::get('test/mail', function() {
+    Mail::to('email@gmail.com')->send(new WelcomeMail());
+    return new WelcomeMail();
+});
 Route::get('dangky', [App\Http\Controllers\XacThucController::class, 'dangkyKhach'])->name('dangkykhach');
 Route::post('dangky', [App\Http\Controllers\XacThucController::class, 'formDangky'])->name('formdangky');
 Route::get('dangxuat', [App\Http\Controllers\XacThucController::class, 'dangxuat'])->name('dangxuatkhach');
@@ -25,7 +31,7 @@ Route::group(['as' => 'khachhang.'], function() {
 Route::group(['as' => 'trangchu.'], function() {
     Route::get('/', [App\Http\Controllers\TrangChuController::class, 'index'])->name('index');
     Route::get('/xemgiohang', [App\Http\Controllers\TrangChuController::class, 'xemgiohang'])->name('xemgiohang');
-    Route::get('/{id}/', [App\Http\Controllers\TrangChuController::class, 'show'])->name('show');
+    Route::get('/{id}', [App\Http\Controllers\TrangChuController::class, 'show'])->name('show');
     Route::post('{id}/themvaogiohang', [App\Http\Controllers\TrangChuController::class, 'themvaogiohang'])->name('themvaogiohang');
     Route::group(['prefix' => 'giohang'], function() {
         Route::post('{id}/capnhat', [App\Http\Controllers\TrangChuController::class, 'capnhatGiohang'])->name('capnhatsoluong');
